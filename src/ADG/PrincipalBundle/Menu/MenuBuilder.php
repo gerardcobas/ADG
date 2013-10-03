bb7dfcc33f96a971e547b3bbef71e98ba342327c
Gerard <gerardjevybcn@gmail.com>
2013-10-04 00:32:28 +0200

    Added KnpMenuBundle with proper css

---
+++ src/ADG/PrincipalBundle/Menu/MenuBuilder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ADG\PrincipalBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class MenuBuilder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+		$menu = $factory->createItem('root');
+		$menu->setChildrenAttribute('class', 'nav navbar-nav');
+
+        $menu->addChild('Principal', array('route' => 'index'))->setAttribute('icon', 'icon-home');
+        $menu->addChild('Arxiu', array('route' => 'index'))->setAttribute('icon', 'icon-book');
+        
+        //$menu->addChild('Iniciar sessió', array('route' => 'index'))->setAttribute('icon', 'icon-user')->setAttribute('class', 'nav navbar-right');;
+        
+     
+        // ... add more children
+
+        return $menu;
+    }
+    
+    
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+    	$menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+    
+    	/*
+    	 You probably want to show user specific information such as the username here. That's possible! Use any of the below methods to do this.
+    
+    	if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+    	$username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+    
+    	*/
+    	$menu->addChild('Iniciar sessió', array('route' => 'index'))->setAttribute('icon', 'icon-user');
+    	
+    	
+
+    	return $menu;
+    }
+    
+}
