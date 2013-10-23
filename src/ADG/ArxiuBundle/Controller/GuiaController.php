c9e16c62683161c90ce161ba0d3d25a932d31eb3
Gerard <gerardjevybcn@gmail.com>
2013-10-23 16:16:37 +0200

    ArxiuGuia: Added all info display

--- src/ADG/ArxiuBundle/Controller/GuiaController.php
+++ src/ADG/ArxiuBundle/Controller/GuiaController.php
@@ -22,28 +22,39 @@ class GuiaController extends Controller
     public function searchAction($id)
     {
     	$em = $this->getDoctrine()->getManager();
-    	
-    	$currentId = $id;
-    	
+
     	$repFons = $em->getRepository('ArxiuBundle:GuiaFons');
     	$allFons = $repFons->findAll();
-
-    	$repSub = $em->getRepository('ArxiuBundle:GuiaSubfons');
-    	$subFons = $repSub->findByLike($id);
-    	
-    	$repGrup = $em->getRepository('ArxiuBundle:GuiaGrup');
-    	$grups = $repGrup->findByLike($id);
-    	
+		$subFons=null;
+		$grups=null;
+		
     	$parts = explode('.', $id);
     	$tam=sizeof($parts);
     	$info=null;
-    	if($tam==2){
-    		$info=$repFons->find($id);
+
+    	if($tam >= 2){
+    		$repSub = $em->getRepository('ArxiuBundle:GuiaSubfons');
+    		$subFons = $repSub->findByLike($id);
+    		if($tam==2){
+    			$info=$repFons->find($id);
+    		}
+    		else if($tam==3){
+    			$info=$repSub->find($id);
+    		}
+    	}
+    	if($tam >= 3){
+    		$repGrup = $em->getRepository('ArxiuBundle:GuiaGrup');
+    		$grups = $repGrup->findByLike($id);
+    		if($tam==4){
+    			$info=$repGrup->find($id);
+    		}
     	}
     	
 
+
+
     	return $this->render('ArxiuBundle:Guia:guia.html.twig',
-    			array('info' => $info, 'allFons' => $allFons, 'currentId' => $currentId, 'subFons' => $subFons, 'grups' => $grups)
+    			array('info' => $info, 'allFons' => $allFons, 'currentId' => $id, 'subFons' => $subFons, 'grups' => $grups)
     	);
     }
     
