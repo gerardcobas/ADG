c2b17eabbd06c663eece9a522a89bcf316b3772f
Gerard <gerardjevybcn@gmail.com>
2013-10-25 17:14:44 +0200

    ArxiuGuia working

--- src/ADG/PrincipalBundle/Controller/AdminGuiaController.php
+++ src/ADG/PrincipalBundle/Controller/AdminGuiaController.php
@@ -45,7 +45,7 @@ class AdminGuiaController extends Controller
     		if ($b===true) {
     			$this->get('session')->getFlashBag()->add(
     					'error',
-    					"Ja existeix!"
+    					"Error! Ja existeix aquest nivell!"
     			);
     			return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
     		}
@@ -93,7 +93,7 @@ class AdminGuiaController extends Controller
     			
     			$this->get('session')->getFlashBag()->add(
     					'success',
-    					"Afegit correctament!"
+    					"Entrada afegida correctament!"
     			);
     			return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
     		}
@@ -103,22 +103,210 @@ class AdminGuiaController extends Controller
     	return $this->redirect($this->generateUrl('guia'));
 	}
 	
+	/**
+	 * Obre formulari per afegir un subnivell a partir del nivell de l'indicat i amb els parametres d'aquest.
+	 */
 	public function subnivellAction($id)
 	{
-		/*comprovara si ja existeix aquesta entrada!*/
-		return $this->render('PrincipalBundle:AdminGuia:subnivell.html.twig');
+		list($info,$tam)=self::buscaInfo($id);
+		return $this->render('PrincipalBundle:AdminGuia:subnivell.html.twig',
+				array('info' => $info, 'tam'=> $tam)
+		);
 	}
 	
+	/**
+	 * Crea un nou subnivell a partir de l'informacio del formulari.
+	 * Comprovarà si existeix en primer lloc.
+	 */
+	public function subnivellConfirmarAction()
+	{
+		$request = $this->getRequest();
+		if ($request->isMethod('POST')) {
+		
+			$part1 = $request->request->get('nivell');
+			$part2=$request->request->get('suffix');
+			$nivell=$part1.$part2;
+			$b=self::comprovaExistencia($nivell);
+		
+			if ($b===true) {
+				$this->get('session')->getFlashBag()->add(
+						'error',
+						"Error! Ja existeix aquest nivell!"
+				);
+				return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
+			}
+			else{
+				$em = $this->getDoctrine()->getManager();
+		
+				$entity= self::creaEntitat($nivell);
+				 
+				$entity->setNivell($nivell);
+				$entity->setTitol($request->request->get('titol'));
+				$entity->setCodi($request->request->get('codi'));
+				$entity->setData($request->request->get('data'));
+				$entity->setVolum($request->request->get('volum'));
+				 
+				$entity->setNomProductor($request->request->get('nomProductor'));
+				$entity->setHistoriaProductor($request->request->get('historiaProductor'));
+				$entity->setHistoriaArxivistica($request->request->get('historiaArxivistica'));
+				$entity->setDadesIngres($request->request->get('dadesIngres'));
+				 
+				$entity->setAbast($request->request->get('abast'));
+				$entity->setOrganitzacio($request->request->get('organitzacio'));
+				$entity->setInformacioUtilitzacio($request->request->get('informacioUtilitzacio'));
+				$entity->setIncrements($request->request->get('increments'));
+				 
+				$entity->setCondicionsAcces($request->request->get('condicionsAcces'));
+				$entity->setCondicionsReproduccio($request->request->get('condicionsReproduccio'));
+				$entity->setLlengues($request->request->get('llengues'));
+				$entity->setCaracteristiques($request->request->get('caracteristiques'));
+				$entity->setInstruments($request->request->get('intruments'));
+				 
+				$entity->setExistenciaOriginals($request->request->get('existenciaOriginals'));
+				$entity->setExistenciaReproduccions($request->request->get('existenciaReproduccio'));
+				$entity->setDocumentacio($request->request->get('documentacio'));
+				$entity->setBibliografia($request->request->get('bibliografia'));
+				 
+				$entity->setNotes($request->request->get('notes'));
+				 
+				$entity->setAutoria($request->request->get('autoria'));
+				$entity->setFonts($request->request->get('fonts'));
+				$entity->setRegles($request->request->get('regles'));
+				 
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($entity);
+				$em->flush();
+				 
+				$this->get('session')->getFlashBag()->add(
+						'success',
+						"Entrada afegida correctament!"
+				);
+				return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
+			}
+		
+		
+		}
+		return $this->redirect($this->generateUrl('guia'));
+	}
+	
+	
+	/**
+	 * Obre formulari per afegir editar el nivell indicat.
+	 */
 	public function editarAction($id)
 	{
-		return $this->render('PrincipalBundle:AdminGuia:editar.html.twig');
+		list($info,$tam)=self::buscaInfo($id);
+		return $this->render('PrincipalBundle:AdminGuia:editar.html.twig',
+				array('info' => $info, 'tam'=> $tam)
+		);
 	}
+	
+	/**
+	 * Edita l'entitat original amb les modificacions fetes.
+	 * Comprovarà si existeix en primer lloc.
+	 */
+	public function editarConfirmarAction()
+	{
+		$request = $this->getRequest();
+		if ($request->isMethod('POST')) {
+	
+			$original = $request->request->get('original');
+			$part1 = $request->request->get('nivell');
+			$part2=$request->request->get('suffix');
+			$nivell=$part1.$part2;
+			$b=self::comprovaExistencia($nivell);
+	
+			if ($b===true && $nivell!=$original) {
+				$this->get('session')->getFlashBag()->add(
+						'error',
+						"Error! Aquest nou nivell ja existeix!"
+				);
+				return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
+			}
+			else{
+				$em = $this->getDoctrine()->getManager();
+	
+				list($entity,$tam)=self::buscaInfo($original);
+					
+				$entity->setNivell($nivell);
+				$entity->setTitol($request->request->get('titol'));
+				$entity->setCodi($request->request->get('codi'));
+				$entity->setData($request->request->get('data'));
+				$entity->setVolum($request->request->get('volum'));
+					
+				$entity->setNomProductor($request->request->get('nomProductor'));
+				$entity->setHistoriaProductor($request->request->get('historiaProductor'));
+				$entity->setHistoriaArxivistica($request->request->get('historiaArxivistica'));
+				$entity->setDadesIngres($request->request->get('dadesIngres'));
+					
+				$entity->setAbast($request->request->get('abast'));
+				$entity->setOrganitzacio($request->request->get('organitzacio'));
+				$entity->setInformacioUtilitzacio($request->request->get('informacioUtilitzacio'));
+				$entity->setIncrements($request->request->get('increments'));
+					
+				$entity->setCondicionsAcces($request->request->get('condicionsAcces'));
+				$entity->setCondicionsReproduccio($request->request->get('condicionsReproduccio'));
+				$entity->setLlengues($request->request->get('llengues'));
+				$entity->setCaracteristiques($request->request->get('caracteristiques'));
+				$entity->setInstruments($request->request->get('intruments'));
+					
+				$entity->setExistenciaOriginals($request->request->get('existenciaOriginals'));
+				$entity->setExistenciaReproduccions($request->request->get('existenciaReproduccio'));
+				$entity->setDocumentacio($request->request->get('documentacio'));
+				$entity->setBibliografia($request->request->get('bibliografia'));
+					
+				$entity->setNotes($request->request->get('notes'));
+					
+				$entity->setAutoria($request->request->get('autoria'));
+				$entity->setFonts($request->request->get('fonts'));
+				$entity->setRegles($request->request->get('regles'));
+					
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($entity);
+				$em->flush();
+					
+				$this->get('session')->getFlashBag()->add(
+						'success',
+						"Entrada editada correctament!"
+				);
+				return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
+			}
+	
+	
+		}
+		return $this->redirect($this->generateUrl('guia'));
+	}
+	
+	
 	
 	public function eliminarAction($id)
 	{
-		return $this->render('PrincipalBundle:AdminGuia:eliminar.html.twig');
+		list($info,$tam)=self::buscaInfo($id);
+		return $this->render('PrincipalBundle:AdminGuia:eliminar.html.twig',
+				array('info' => $info, 'tam'=> $tam)
+		);
 	}
 	
+	public function eliminarConfirmarAction()
+	{
+		$request = $this->getRequest();
+		if ($request->isMethod('POST')) {
+		
+			$nivell = $request->request->get('nivell');
+			list($entity,$tam)=self::buscaInfo($nivell);
+			
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($entity);
+			$em->flush();
+			
+			$this->get('session')->getFlashBag()->add(
+					'success',
+					"Nivell eliminat correctament!"
+			);
+			return $this->redirect($this->generateUrl('guia'));
+		}
+		return $this->redirect($this->generateUrl('guia'));
+	}
 	
 	/**
 	 * Busca la informació de l'entitat corresponent al identificador
