a6557526a8cc031a74ba0fe9a4b25f0ed9ba0668
Gerard <gerardjevybcn@gmail.com>
2013-10-25 12:01:37 +0200

    AdminGuia nou persistence

--- src/ADG/ArxiuBundle/Entity/GuiaFons.php
+++ src/ADG/ArxiuBundle/Entity/GuiaFons.php
@@ -17,7 +17,6 @@ class GuiaFons
      *
      * @ORM\Column(name="nivell", type="string", length=255, nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $nivell;
 
--- src/ADG/PrincipalBundle/Controller/AdminGuiaController.php
+++ src/ADG/PrincipalBundle/Controller/AdminGuiaController.php
@@ -7,7 +7,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\SecurityContext;
-use Adg\ArxiuBundle\Entity\GuiaFons;
+use ADG\ArxiuBundle\Entity\GuiaFons;
+use ADG\ArxiuBundle\Entity\GuiaSubfons;
+use ADG\ArxiuBundle\Entity\GuiaGrup;
+use ADG\ArxiuBundle\Entity\GuiaSerie;
+use ADG\ArxiuBundle\Entity\GuiaUcomposta;
+use ADG\ArxiuBundle\Entity\GuiaUsimple;
+use ADG\ArxiuBundle\Entity\GuiaUinstalacio;
 
 class AdminGuiaController extends Controller
 {
@@ -22,6 +28,10 @@ class AdminGuiaController extends Controller
 		);
 	}
 	
+	/**
+	 * Crea la nova entrada a partir de l'informacio del formulari.
+	 * Comprovarà si existeix en primer lloc.
+	 */
 	public function nouConfirmarAction()
 	{
 		$request = $this->getRequest();
@@ -33,7 +43,6 @@ class AdminGuiaController extends Controller
     		$b=self::comprovaExistencia($nivell);
     		
     		if ($b===true) {
-    			
     			$this->get('session')->getFlashBag()->add(
     					'error',
     					"Ja existeix!"
@@ -41,6 +50,46 @@ class AdminGuiaController extends Controller
     			return $this->redirect($this->generateUrl('search', array('id' => $nivell)));
     		}
     		else{
+    			$em = $this->getDoctrine()->getManager();
+
+    			$entity= self::creaEntitat($nivell);
+    			
+    			$entity->setNivell($nivell);
+    			$entity->setTitol($request->request->get('titol'));
+    			$entity->setCodi($request->request->get('codi'));
+    			$entity->setData($request->request->get('data'));
+    			$entity->setVolum($request->request->get('volum'));
+    			
+    			$entity->setNomProductor($request->request->get('nomProductor'));
+    			$entity->setHistoriaProductor($request->request->get('historiaProductor'));
+    			$entity->setHistoriaArxivistica($request->request->get('historiaArxivistica'));
+    			$entity->setDadesIngres($request->request->get('dadesIngres'));
+    			
+    			$entity->setAbast($request->request->get('abast'));
+    			$entity->setOrganitzacio($request->request->get('organitzacio'));
+    			$entity->setInformacioUtilitzacio($request->request->get('informacioUtilitzacio'));
+    			$entity->setIncrements($request->request->get('increments'));
+    			
+    			$entity->setCondicionsAcces($request->request->get('condicionsAcces'));
+    			$entity->setCondicionsReproduccio($request->request->get('condicionsReproduccio'));
+    			$entity->setLlengues($request->request->get('llengues'));
+    			$entity->setCaracteristiques($request->request->get('caracteristiques'));
+    			$entity->setInstruments($request->request->get('intruments'));
+    			
+    			$entity->setExistenciaOriginals($request->request->get('existenciaOriginals'));
+    			$entity->setExistenciaReproduccions($request->request->get('existenciaReproduccio'));
+    			$entity->setDocumentaciot($request->request->get('documentacio'));
+    			$entity->setBibliografia($request->request->get('bibliografia'));
+    			
+    			$entity->setNotes($request->request->get('notes'));
+    			
+    			$entity->setAutoria($request->request->get('autoria'));
+    			$entity->setFonts($request->request->get('fonts'));
+    			$entity->setRegles($request->request->get('regles'));
+    			
+    			$em = $this->getDoctrine()->getManager();
+    			$em->persist($entity);
+    			$em->flush();
     			
     			$this->get('session')->getFlashBag()->add(
     					'success',
@@ -70,6 +119,10 @@ class AdminGuiaController extends Controller
 		return $this->render('PrincipalBundle:AdminGuia:eliminar.html.twig');
 	}
 	
+	
+	/**
+	 * Busca la informació de l'entitat corresponent al identificador
+	 */
 	private function buscaInfo($id){
 
 		$em = $this->getDoctrine()->getManager();
@@ -111,6 +164,9 @@ class AdminGuiaController extends Controller
 		return array($info,$tam);
 	}
 	
+	/**
+	 * Comprova l'existencia del corresponent identificador
+	 */
 	private function comprovaExistencia($id){
 	
 		$em = $this->getDoctrine()->getManager();
@@ -157,6 +213,37 @@ class AdminGuiaController extends Controller
 		}
 	
 		return false;
+	}
+	
+	/**
+	 * Crea una entitat buida com la del indentificador donat.
+	 */
+	private function creaEntitat($id){
+		$parts = explode('.', $id);
+		$tam=sizeof($parts);
+		$entitat=null;
+		if($tam==2){
+			$entitat= new GuiaFons();
+		}
+		else if($tam==3){
+			$entitat= new GuiaSubfons();
+		}
+		else if($tam==4){
+			$entitat= new GuiaGrup();
+		}
+		else if($tam==5){
+			$entitat= new GuiaSerie();
+		}
+		else if($tam==6){
+			$entitat= new GuiaUcomposta();
+		}
+		else if($tam==7){
+			$entitat= new GuiaUsimple();
+		}
+		else if($tam==8){
+			$entitat= new GuiaUinstalacio();
+		}
+		return $entitat;
 	}
 		
 }
