cdd7133f6fad5e7d8936b6ad918bfea4c0b5d6ea
Gerard <gerardjevybcn@gmail.com>
2013-10-31 23:00:11 +0100

    More Fons changes

--- src/ADG/ArxiuBundle/Controller/FonsController.php
+++ src/ADG/ArxiuBundle/Controller/FonsController.php
@@ -16,25 +16,74 @@ class FonsController extends Controller
     public function selectAction($seleccio)
     {
     	$tipus=self::obteTipus($seleccio);
-    	
-    	//obtenir tota la taula en arxius / monges / capellans / seminas
+    	$info=null;
+    	//obtenir tota la taula de monges / capellans / seminaristes
+    	if ($tipus=="capellans") {
+    		$em = $this->getDoctrine()->getManager();
+    		$rep = $em->getRepository('ArxiuBundle:FonsCapellans');
+    		$info=$rep->findAll();
+    	}
+    	elseif ($tipus=="monges") {
+    		$em = $this->getDoctrine()->getManager();
+    		$rep = $em->getRepository('ArxiuBundle:FonsMonges');
+    		$info=$rep->findAll();
+    	}
+    	elseif ($tipus=="seminaristes") {
+    		$em = $this->getDoctrine()->getManager();
+    		$rep = $em->getRepository('ArxiuBundle:FonsSeminaristes');
+    		$info=$rep->findAll();
+    	}
     	
     	return $this->render('ArxiuBundle:Fons:fons.html.twig',
     			array('seleccio' => $seleccio, 'tipus' => $tipus, 'cercaParaula'=>null, 
-    					'cercaNom'=>null, 'cercaLloc'=>null,'cercaData'=>null,'info' => null)
+    					'cercaNom'=>null, 'cercaLloc'=>null,'cercaData'=>null,'cercaCongregacio'=>null, 'info' => $info)
     	);
     }
 
     public function cercarAction($seleccio)
     {
     	$tipus=self::obteTipus($seleccio);
-    	//select cercas i retornarles
     	
+    	//select cercas i retornarles
+    	$request = $this->getRequest();
+    	if ($request->isMethod('POST')) {
+    		$cercaParaula = $request->request->get('cercaParaula');
+    		$cercaNom = $request->request->get('cercaNom');
+    		$cercaLloc = $request->request->get('cercaLloc');
+    		$cercaData = $request->request->get('cercaData');
+    		$cercaCongregacio = $request->request->get('cercaCongregacio');
+    	}
+    	
+    	$info=null;
     	//mirar kines estan plenes i fer cerca a BD
+    	if ($tipus=="arxiu") {
+    		//query amb data i paraula
+    	}
+    	else if($tipus=="capellans"){
+    		//nom, lloc, data
+    	}    	
+    	else if($tipus=="monges"){
+    		//nom, lloc, congregacio
+    	}
+    	else if($tipus=="seminaristes"){
+    		//nom, data
+    	}    	    	
+    	else{
+    		//paraula
+    	}
     	
     	return $this->render('ArxiuBundle:Fons:fons.html.twig',
-    			array('seleccio' => $seleccio, 'tipus' => $tipus, 'cercaParaula'=>null, 
-    					'cercaNom'=>null, 'cercaLloc'=>null,'cercaData'=>null, 'info' => null)
+    			array('seleccio' => $seleccio, 'tipus' => $tipus, 'cercaParaula'=>$cercaParaula, 
+    					'cercaNom'=>$cercaNom, 'cercaLloc'=>$cercaLloc,'cercaData'=>$cercaData, 'cercaCongregacio'=>$cercaCongregacio, 'info' => $info)
+    	);
+    }
+    
+    public function detallAction($tipus, $id)
+    {
+    	
+    	//obtindra el tipus i el num o id en cas de liberden per pasar la info a mostrar
+    	return $this->render('ArxiuBundle:Fons:detall.html.twig',
+    			array('tipus' => $tipus, 'info' => null)
     	);
     }
     
@@ -71,5 +120,7 @@ class FonsController extends Controller
     	}
     	return $tipus;
     }
+    
+    
     
 }
