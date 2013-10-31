71202ffbd6e6d4282ab02ad77b74a7a7ad5f70c1
Gerard <gerardjevybcn@gmail.com>
2013-10-31 13:52:40 +0100

    Starting fons cerca

--- src/ADG/ArxiuBundle/Controller/FonsController.php
+++ src/ADG/ArxiuBundle/Controller/FonsController.php
@@ -15,10 +15,52 @@ class FonsController extends Controller
     
     public function selectAction($seleccio)
     {
+    	$tipus=self::obteTipus($seleccio);
     	return $this->render('ArxiuBundle:Fons:fons.html.twig',
-    			array('seleccio' => $seleccio, 'info' => null)
+    			array('seleccio' => $seleccio, 'tipus' => $tipus, 'cercaParaula'=>null, 'info' => null)
     	);
     }
-  
+
+    public function cercarAction($seleccio)
+    {
+    	$tipus=self::obteTipus($seleccio);
+    	return $this->render('ArxiuBundle:Fons:fons.html.twig',
+    			array('seleccio' => $seleccio, 'tipus' => $tipus, 'cercaParaula'=>null, 'info' => null)
+    	);
+    }
+    
+    
+    
+    /**
+     * Calcula el tipus a partir de la seleccio
+     */
+    private function obteTipus($seleccio){
+   		$tipus=null;
+    	if ($seleccio == "curia" or $seleccio == "almoina" or $seleccio == "stfeliu" or $seleccio == "besalu" or $seleccio == "llado" or $seleccio == "cadins") {
+    		$tipus="arxius";
+    	}
+    	elseif ($seleccio == "mitra"){
+    		$tipus="mitra";
+    	}
+    	elseif ($seleccio == "liberden"){
+    		$tipus="liberden";
+    	}
+    	elseif ($seleccio == "capellans"){
+    		$tipus="capellans";
+    	}
+    	elseif ($seleccio == "monges"){
+    		$tipus="monges";
+    	}
+    	elseif ($seleccio == "seminaristes"){
+    		$tipus="seminaristes";
+    	}
+    	elseif ($seleccio == "testaments" or $seleccio == "resolucions" or $seleccio == "definicions" or $seleccio == "definicions"){
+    		$tipus="testaments";
+    	}
+    	else{
+    		$tipus="fons";
+    	}
+    	return $tipus;
+    }
     
 }
